d785432ae7888111a188c5696eafd47aec5eba5f
TimDupontPXL <tim.dupont@pxl.be>
2020-09-27 15:43:28 +0200

    Fixed permision issue.

--- Commands/bin/install_turtlebot3.sh
+++ Commands/bin/install_turtlebot3.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+(sudo chown user:user /home/user/Ptojects/catkin_ws/src)
 (cd /home/user/Projects/catkin_ws/src/; git clone https://github.com/ROBOTIS-GIT/turtlebot3_msgs.git)
 (cd /home/user/Projects/catkin_ws/src/; git clone https://github.com/ROBOTIS-GIT/turtlebot3.git)
 (cd /home/user/Projects/catkin_ws/src/; git clone https://github.com/ROBOTIS-GIT/turtlebot3_simulations.git)
