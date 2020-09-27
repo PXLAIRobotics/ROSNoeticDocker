bca9a4cfeb4c9f6c422d78584a23c198140a3f03
TimDupontPXL <tim.dupont@pxl.be>
2020-09-27 15:41:20 +0200

    Added catkin_make command to install script.

--- Commands/bin/install_turtlebot3.sh
+++ Commands/bin/install_turtlebot3.sh
@@ -3,3 +3,4 @@
 (cd /home/user/Projects/catkin_ws/src/; git clone https://github.com/ROBOTIS-GIT/turtlebot3_msgs.git)
 (cd /home/user/Projects/catkin_ws/src/; git clone https://github.com/ROBOTIS-GIT/turtlebot3.git)
 (cd /home/user/Projects/catkin_ws/src/; git clone https://github.com/ROBOTIS-GIT/turtlebot3_simulations.git)
+(cd /home/user/Projects/catkin_ws; catkin_make)
