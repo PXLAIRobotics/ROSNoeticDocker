d7f901bd48ac6a9682981edac41218e530335313
TimDupontPXL <tim.dupont@pxl.be>
2020-09-27 15:32:26 +0200

    Added install turtlebot3 script.

---
+++ Commands/bin/install_turtlebot3.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+(cd /home/user/Projects/catkin_ws/src/; git clone https://github.com/ROBOTIS-GIT/turtlebot3_msgs.git)
+(cd /home/user/Projects/catkin_ws/src/; git clone https://github.com/ROBOTIS-GIT/turtlebot3.git)
+(cd /home/user/Projects/catkin_ws/src/; git clone https://github.com/ROBOTIS-GIT/turtlebot3_simulations.git)
