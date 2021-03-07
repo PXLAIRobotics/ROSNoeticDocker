368c4bd7267c947dcbda82fdc786ee53d7a6e7d1
TimDupontPXL <tim.dupont@pxl.be>
2021-03-07 22:45:49 +0100

    Removed unnecessary Turtlebot3 install script.

--- Commands/bin/install_turtlebot3.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-(sudo chown user:user /home/user/Projects/catkin_ws/src)
-(cd /home/user/Projects/catkin_ws/src/; git clone https://github.com/ROBOTIS-GIT/turtlebot3_msgs.git)
-(cd /home/user/Projects/catkin_ws/src/; git clone https://github.com/ROBOTIS-GIT/turtlebot3.git)
-(cd /home/user/Projects/catkin_ws/src/; git clone https://github.com/ROBOTIS-GIT/turtlebot3_simulations.git)
-(cd /home/user/Projects/catkin_ws; catkin_make)
